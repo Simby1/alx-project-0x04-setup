eb4f8a21108af745a1df060e6b55d90c2db1e99e
Simby1 <kimsimby@gmail.com>
2025-09-28 18:30:53 +0100

    feat: Implementing a share layout

---
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen pt-24"> {/* Added pt-24 to offset fixed header height */}
+      <Header />
+      <main className="flex-grow">{children}</main> {/* flex-grow ensures main takes up available space */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
